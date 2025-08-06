6f7a929783c8fc3119685ae8998a1f2b93ffa63e
EugeneO0o <chenyanjin.eugene@gmail.com>
2025-08-06 10:38:02 +0800

    upload data

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2290079.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 243383747, 243383730, 243383749, 243383715, 243383765, 243383761, 243383707, 243700300, 243700298, 243485411 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/243383734/1/basis.wasm.js', 'wasmUrl' : 'files/assets/243383743/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/243383752/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/243383740/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/243383728/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/243383753/1/ammo.js', 'preload' : true},
+];
